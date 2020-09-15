f2c503a2386a6056f0ec07bea6d370dd3c2c649f
James Lafferty <jameslafferty@Jamess-MBP.home>
2020-09-14 20:49:27 -0700

    Some moderate enhancements to the stopwatch

--- packages/stop-watch/src/StopWatch.tsx
+++ packages/stop-watch/src/StopWatch.tsx
@@ -1,12 +1,14 @@
-import React, { FormEvent, useState } from 'react';
-
 import { Counter } from '@ketchupy/counter';
+import React, { FormEvent, useCallback, useRef, useState } from 'react';
+
 
 export const StopWatch: React.FC = () => {
   const [ isPaused, setIsPaused ] = useState(true);
-  const onSubmit = (event: FormEvent) => {
+  const onSubmit = useCallback((event: FormEvent) => {
     event.preventDefault();
-  };
+  }, []);
+  const pause = useCallback(() => setIsPaused(() => false), [setIsPaused]);
+  const play = useCallback(() => setIsPaused(() => true), [setIsPaused]);
   return (
     <form
       data-testid='StopWatch'
@@ -14,14 +16,14 @@ export const StopWatch: React.FC = () => {
     >
       <button
         disabled={!isPaused}
-        onClick={() => setIsPaused(() => false)}
+        onClick={pause}
         type='button'
       >
         <span>Start</span>
       </button>
       <button
         disabled={isPaused}
-        onClick={() => setIsPaused(() => true)}
+        onClick={play}
         type='button'
       >
         <span>Pause</span>
